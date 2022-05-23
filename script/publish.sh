420f8e961bf239acede61fd724c037687721e354
marcus.cheng <marcus.cheng@hextrust.com>
2022-05-23 14:51:23 +0800

    support go migration

---
+++ script/publish.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+usage() {
+    if [[ -z "$1" ]]; then
+        # General usage message 
+        echo "Please provide PACKAGE_REGISTRY_USER and PACKAGE_REGISTRY_PASSWORD"
+    else
+        echo "$1"
+    fi
+    exit 1
+}
+
+echo "$0"
+PACKAGE_REGISTRY_USER="$PACKAGE_REGISTRY_USER"
+PACKAGE_REGISTRY_PASSWORD="$PACKAGE_REGISTRY_PASSWORD"
+[[ ! -z "${PACKAGE_REGISTRY_USER}" ]] || usage
+[[ ! -z "${PACKAGE_REGISTRY_PASSWORD}" ]] || usage
+
+echo "Packaging helm chart..."
+helm package .
+PACKAGE=$(ls | grep -i '^generic-helm-chart.*tgz$')
+[[ ! -z "${PACKAGE}" ]] || usage "Helm package not found, exit 1"
+
+echo "Uploading helm chart..."
+RESP=$(curl --request POST --form "chart=@${PACKAGE}" --user $USER:$PASSWORD https://gitlab.int.hextech.io/api/v4/projects/645/packages/helm/api/alpha/charts)
+echo $RESP
+if [[ $RESP == *"20"* ]]; then
+    echo "Upload succeed"
+else
+    usage "Upload failed, exit 1"
+fi
